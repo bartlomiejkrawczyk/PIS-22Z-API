27beb41dbe2e6e78c116fcd9fb24567e7a6fd7ea
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-01-10 21:45:14 +0100

    [CODE REVIEW] Delete unnecessary parts of code

--- src/main/java/com/example/api/users/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.users.service;
-
-import com.example.model.User;
-import reactor.core.publisher.Mono;
-
-public interface UserService {
-    Mono<String> getPasswordById(int id);
-    Mono<User> saveUser(User user);
-}
